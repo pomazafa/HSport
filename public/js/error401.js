528daa9c12ab6fadcd2848be73499adf5e31d274
Polina <p.duben@mail.ru>
2020-05-11 22:22:44 +0300

    fixed references

--- public/js/error401.js
+++ public/js/error401.js
@@ -3,7 +3,7 @@ module.exports = function error401(request, response)
     response.render('message.hbs', {
             Title: "Ошибка авторизации",
             message: "Чтобы выполнить это действие, необходимо авторизоваться",
-            buttonAction: "window.location.href = 'http://localhost:3000/entry/exit'",
+            buttonAction: "window.location.href = '/entry/exit'",
             buttonValue: "Авторизоваться"
         });
 }
